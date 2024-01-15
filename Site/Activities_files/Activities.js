83e1813ef76bf3d9a041b20d3e6e1aa072b317e9
João Paulo Fernandes <jpf@dei.uc.pt>
2024-01-15 21:01:52 +0400

    adding folder

---
+++ Site/Activities_files/Activities.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20240115
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Activities_files/ActivitiesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
